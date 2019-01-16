b7b6970ec6339a436fd93d7023351afd3a91670a
Frank Rosendorf <frank.rosendorf1@gmail.com>
2019-01-16 12:22:50 -0500

    Add urlpatterns and minerals_list template

--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Mineral
 
 # Create your views here.
+
+
+def mineral_list(request):
+    minerals = Mineral.objects.all()
+    return render(request, 'mineral_list.html', {'minerals': minerals})
